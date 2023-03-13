1bc8034d0088dc35665cd8d9a4a31308c80d1fdb
Benjamin <none>
2023-03-13 19:45:18 +0100

    small utility to calculate wer

---
+++ calc_wer.py
@@ -0,0 +1,56 @@
+import jiwer
+from somajo import SoMaJo
+from jiwer import cer, wer
+import argparse
+
+punctuation = '.,!?;:-_"\''
+
+def to_word_list(tokenizer, paragraphs, remove_punctuation=False):
+
+    paragraph_ids = paragraphs.keys()
+    paragraphs = paragraphs.values()
+
+    if remove_punctuation: 
+        paragraph_tokens = [[token.text for token in sentence if token.text not in punctuation] for sentence in tokenizer.tokenize_text(paragraphs)]
+    else:
+        paragraph_tokens = [[token.text for token in sentence] for sentence in tokenizer.tokenize_text(paragraphs)]
+
+    return dict(zip(paragraph_ids, paragraph_tokens))
+
+def calculate_word_error_rate(kaldi_test_text_file, decoded_text_file, remove_punctuation=False, split_camel_case=True):
+    tokenizer = SoMaJo(language="de_CMC", split_camel_case=split_camel_case)
+
+    test_utterances = {}
+    decoded_utterances = {}
+
+    with open(kaldi_test_text_file) as kaldi_test_text_file_in:
+        for line in kaldi_test_text_file_in:
+            utterance_id, transcription = line.strip().split(' ', 1)
+            test_utterances[utterance_id] = transcription
+
+    with open(decoded_text_file) as decoded_text_file_in:
+        for line in decoded_text_file_in:
+            utterance_id, transcription = line.strip().split(' ', 1)
+            decoded_utterances[utterance_id] = transcription
+
+    test_utterances = to_word_list(tokenizer, test_utterances, remove_punctuation)
+    decoded_utterances = to_word_list(tokenizer, decoded_utterances, remove_punctuation)
+    decoded_utterances_ids = decoded_utterances.keys()
+
+    test_utterances_list = [' '.join(test_utterances[utt_id]) for utt_id in decoded_utterances_ids]
+    decoded_utterances_list = [' '.join(decoded_utterances[utt_id]) for utt_id in decoded_utterances_ids]
+
+
+    print('CER:', cer(test_utterances_list, decoded_utterances_list))
+    print('WER:', wer(test_utterances_list, decoded_utterances_list))
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser() 
+    parser.add_argument('--remove_punctuation', dest='remove_punctuation',
+                        help='Remove punctuation before calculating metrics', action='store_true')
+    parser.add_argument('testset_text', type=str, help='The test file containing the test set utterances')
+    parser.add_argument('decoded_text', type=str, help='The decoded file containing the decoded utterances')
+
+    args = parser.parse_args()
+
+    calculate_word_error_rate(args.testset_text, args.decoded_text, remove_punctuation=args.remove_punctuation)
