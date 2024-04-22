12a9b266671d6750588c032abf508dce55c925e6
bmilde <bmilde@users.noreply.github.com>
2024-04-22 20:02:32 +0200

    new version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='speechcatcher',
-    version='0.3.1',
+    version='0.3.2',
     author="Benjamin Milde",
     author_email="bmilde@users.noreply.github.com",
     description="Speechcatcher is an open source toolbox for transcribing speech from media files (audio/video).",
